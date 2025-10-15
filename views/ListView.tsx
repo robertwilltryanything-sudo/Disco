356ad79a29c21af289a8cafd1408265c87c29270
robertwilltryanything-sudo <robertwilltryanything@gmail.com>
2025-10-15 09:10:07 +0200

    refactor: Remove unused useCallback hook
    The `useCallback` hook was imported but not utilized within the `ListView.tsx` component. Removing it cleans up the code and removes an unnecessary import.

--- views/ListView.tsx
+++ views/ListView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect, useCallback } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { CD, SortKey, SortOrder } from '../types';
 import CDList from '../components/CDList';
