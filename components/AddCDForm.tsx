ba84ffad43eeec89e22796386828091a0b56e18a
robertwilltryanything-sudo <robertwilltryanything@gmail.com>
2026-03-13 16:59:23 +0100

    feat: Add "Upgradable" attribute to CD and vinyl
    Adds "Upgradable" as an option to the attributes for both CD and vinyl items. This allows users to mark items that can be upgraded, such as a CD with a jewel case that could be replaced with a digipak.

--- components/AddCDForm.tsx
+++ components/AddCDForm.tsx
@@ -28,11 +28,11 @@ interface AddCDFormProps {
 
 const VINYL_MEDIA_CONDITION = ["Hairlines", "Scratched", "Warped"];
 const VINYL_COVER_CONDITION = ["Ringwear", "Seemsplit", "Price Sticker", "Water Damage", "Tear Front", "Cut Out"];
-const VINYL_ATTRIBUTES = ["Gatefold", "180g", "Coloured Vinyl", "Hype Sticker", "Obi Strip", "OIS"];
+const VINYL_ATTRIBUTES = ["Gatefold", "180g", "Coloured Vinyl", "Hype Sticker", "Obi Strip", "OIS", "Upgradable"];
 
 const CD_MEDIA_CONDITION = ["Scratched", "Hairlines", "Sticky"];
 const CD_COVER_CONDITION = ["Replace Case", "Price Sticker", "Tear Front", "Water damage"];
-const CD_ATTRIBUTES = ["Digipak", "Slipcase", "Obi Strip", "Promo"];
+const CD_ATTRIBUTES = ["Digipak", "Slipcase", "Obi Strip", "Promo", "Upgradable"];
 
 const AddCDForm: React.FC<AddCDFormProps> = ({ onSave, cdToEdit, onCancel, prefill, isVinyl, driveSignedIn, onPickFromDrive }) => {
   const [artist, setArtist] = useState('');
--- components/AddWantlistItemForm.tsx
+++ components/AddWantlistItemForm.tsx
@@ -27,11 +27,11 @@ interface AddWantlistItemFormProps {
 
 const VINYL_MEDIA_CONDITION = ["Hairlines", "Scratched", "Warped"];
 const VINYL_COVER_CONDITION = ["Ringwear", "Seemsplit", "Price Sticker", "Water Damage", "Tear Front", "Cut Out"];
-const VINYL_ATTRIBUTES = ["Gatefold", "180g", "Coloured Vinyl", "Hype Sticker", "Obi Strip", "OIS"];
+const VINYL_ATTRIBUTES = ["Gatefold", "180g", "Coloured Vinyl", "Hype Sticker", "Obi Strip", "OIS", "Upgradable"];
 
 const CD_MEDIA_CONDITION = ["Scratched", "Hairlines", "Sticky"];
 const CD_COVER_CONDITION = ["Replace Case", "Price Sticker", "Tear Front", "Water damage"];
-const CD_ATTRIBUTES = ["Digipak", "Slipcase", "Obi Strip", "Promo"];
+const CD_ATTRIBUTES = ["Digipak", "Slipcase", "Obi Strip", "Promo", "Upgradable"];
 
 const AddWantlistItemForm: React.FC<AddWantlistItemFormProps> = ({ onSave, itemToEdit, onCancel, isVinyl, driveSignedIn, onPickFromDrive }) => {
   const [artist, setArtist] = useState('');
